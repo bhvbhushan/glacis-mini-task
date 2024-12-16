5480416e114ac567b5317d35d16f0dccf04b9a75
Bhavya Bhushan <bhvbhushan7@gmail.com>
2024-12-16 11:02:45 +0530

    [INIT] Initial Project setup

---
+++ src/main.ts
@@ -0,0 +1,3 @@
+console.log("Hello from TS");
+const apiKey = import.meta.env.VITE_FLIGHT_API_KEY;
+console.log(apiKey);
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true, // Opens the browser automatically
+  },
+  // Add more configurations as needed
+});
